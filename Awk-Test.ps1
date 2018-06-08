71ab54d2070b402b938dfabada1c7ec5c01759fc
mayursss <34266617+mayursss@users.noreply.github.com>
2018-06-08 09:51:21 -0500

    Add files via upload

---
+++ Awk-Test.ps1
@@ -0,0 +1,53 @@
+﻿    $FileName = 'C:\temp\test.txt'
+    $data= Get-Content $FileName
+    $output1 =""
+    # $action = '/ms458j/ print'
+    # $action = '/ms458j/ print $0'
+    # $action = '/ms458j/ print $1'
+    # $action = '/ms458j/ print $2'
+    # $action = '/ms458j/ print $2,$3'
+    # $action = '/ms458j/ print $1,$2,$3'
+     $action = '/ms458j/ print $5,$6,$7'
+    # $action = '/ms458j/ print $5,$9,$1'
+    # $action = '/ms458j/ print $5,$10,$11' ## will show only value for $5
+    #$action = '/ms458j/ print $5,"\t",$2'   ## will "\t" does not work
+    #$expression = $action -match '(\/\w+\/|\/\w+\s+\/|\/\s+\w+\s+\/)'
+    $action   = $action -replace ',',' ' -split '\s+' ## replace all $ or , to blankspace ' '
+    $command  = "$($action -match 'print')"
+    $argument = $action -match '\$\d' -replace '\$',''
+    #$argument = $argument -replace '\$','' -split '\s+'
+    if ($command -eq 'print'){
+        if($argument -eq 0 -or $argument.Length -eq 0){
+            $data
+        }
+        Elseif($argument.Length -eq 1){
+            foreach ($item in $data){
+                -join $(($item -split '\s+')[$argument[0]],"`t")
+                }
+            }
+        Else{
+            #Write-Error "Argument not valid"
+        } 
+    }
+    if ($argument.Length -ge 2){
+        foreach ($item in $data){
+            $datatojoin =""
+            for($arg=0; $arg -lt $argument.Length; $arg++){
+                $datatojoin += -join "$(($item -split '\s+')[$argument[$arg]])`t"
+            }            
+           $output1 += "$datatojoin`n"
+        }   
+        $output1     
+    }
+   
+                 
+    # $D = foreach ($line in $data){
+    # $info = $line -split "\s+"
+    # $hash = [ordered]@{}
+    #     for ($i=0;$i -lt $($info.Length+1);$i++) {
+    #     $hash.add("$i",$info[$i])
+    #     }
+    # [pscustomobject]$hash
+
+    # }
+    # $D.0;$D.1;
---
+++ Awk.ps1
@@ -0,0 +1,57 @@
+﻿function awk (
+[string[]]$action,
+#[Parameter(Mandatory)]       
+$FileName,
+$NR,
+$FNR,
+$NF,
+$FS,
+$OFS,
+$ARGC,
+$ARGV
+
+)
+{
+    $data= Get-Content $FileName
+    function printString (
+        $opt1
+        ){
+            #$opt1
+            #$opt2 = $Opt1.Split('$')[1]
+            #Write-Host -ForegroundColor Yellow "Printing $opt1"
+                foreach ($item in $data){
+                ($item -split '\s+')[$opt1]
+
+            }
+        }
+        
+        $action = $action -replace '\$|,',' ' -split '\s+'
+        $command = $action[0]
+        $argument= @()
+        $argument = $action -match '\d+'
+        
+
+    if($command -eq 'print' -and $argument -eq $null)
+    {
+        $data
+    }
+    else{
+        printString $argument
+    }
+    
+}
+        
+
+
+#awk '{print $1}' C:\temp\test.txt
+#awk -FileName C:\temp\test.txt
+#awk '{print}' C:\temp\test.txt
+#awk '{print $1}' C:\temp\test.txt
+#awk '{print $2}' C:\temp\test.txt
+#awk '{print $3}' C:\temp\test.txt
+#Get-Content -Path C:\temp\test.txt
+#Get-Help awk
+#awk -action 'print' -FileName 'C:\temp\test.txt'
+awk -action 'print  $8' -FileName 'C:\temp\test.txt'
+#awk -action 'print $1'
+#awk -action 'print $1,$4'
